6d57b321dc1375a9eb66b146447a3c1c4ae9c60f
William Godoy <williamtatim91@gmail.com>
2026-02-10 10:03:40 -0300

    refactor(product composition): add validation quantity required > 0

--- backend/src/main/java/tatim/william/domain/product/ProductComposition.java
+++ backend/src/main/java/tatim/william/domain/product/ProductComposition.java
@@ -3,6 +3,7 @@ package tatim.william.domain.product;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import tatim.william.domain.DomainException;
 import tatim.william.domain.rawmaterial.RawMaterial;
 
 @Entity
@@ -14,15 +15,23 @@ public class ProductComposition {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "raw_material_id", nullable = false)
     private RawMaterial rawMaterial;
 
     @Column(name = "quantity_required", nullable = false)
     private float quantityRequired;
+
+    public void setQuantityRequired(float value){
+        if(value <= 0.0f){
+            throw new DomainException("A quantidade necessária deve ser maior que zero");
+        }
+
+        quantityRequired = value;
+    }
 
 }
